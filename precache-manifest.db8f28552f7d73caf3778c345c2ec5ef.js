480e59273678d5b6f0656cc2ca212d2f834fc622
ivan <ivan@mail.tw>
2019-12-12 15:46:54 +0800

    update

---
+++ precache-manifest.db8f28552f7d73caf3778c345c2ec5ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4c9e7d8f938f72524d",
+    "url": "css/app.6895ac6b.css"
+  },
+  {
+    "revision": "6145439c2d3b8daaa64c",
+    "url": "css/chunk-vendors.af58e7ec.css"
+  },
+  {
+    "revision": "1750b32ed292c226aab4d340d0b5d5c6",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.1750b32e.woff2"
+  },
+  {
+    "revision": "3687425cd00bd7e01a2a62d68a3aab2a",
+    "url": "img/gold-coin.3687425c.png"
+  },
+  {
+    "revision": "e087d9801f3e9eae5ab063eaf8e6e348",
+    "url": "img/sliver-coin.e087d980.png"
+  },
+  {
+    "revision": "7a76825b2fb0f0dc869799dbddbfb8e1",
+    "url": "index.html"
+  },
+  {
+    "revision": "9c4c9e7d8f938f72524d",
+    "url": "js/app.fbe292bb.js"
+  },
+  {
+    "revision": "6145439c2d3b8daaa64c",
+    "url": "js/chunk-vendors.74612322.js"
+  },
+  {
+    "revision": "7fc8ba3e57070e0f3f3601e01acb296a",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
